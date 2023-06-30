5fc42cfc00202b95df1300b70723a11c5f7b6942
Jhonny Lanzuisi <jalb97@gmail.com>
2023-06-30 19:56:42 -0400

    Move to variable fonts and fix typos, bib in dratf

--- draft/build.sh
+++ draft/build.sh
@@ -12,11 +12,11 @@ TEXINPUTS='.:..//:'
 OPENTYPEFONTS='../fonts//:'
 export TEXINPUTS OPENTYPEFONTS
 
-latex_comp_main="tl23 lualatex -halt-on-error main.tex"
-biber_comp_main="tl23 biber --input-directory=.. main"
+latex_comp_main="lualatex -halt-on-error main.tex"
+biber_comp_main="biber --input-directory=.. main"
 
 if [[ -z "$draft" ]]; then
     $latex_comp_main && $biber_comp_main && $latex_comp_main && $latex_comp_main
 else
-    tl23 lualatex -halt-on-error '\PassOptionsToClass{article}{draft} \input{main.tex}'
+    lualatex -halt-on-error '\PassOptionsToClass{article}{draft} \input{main.tex}'
 fi
