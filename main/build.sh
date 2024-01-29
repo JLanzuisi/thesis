9bc6ba22d8303e756365f6d245785a4c150c3229
Jhonny Lanzuisi <jalb97@gmail.com>
2024-01-29 13:53:39 -0400

    Use NewCM font and lualatex

--- main/build.sh
+++ main/build.sh
@@ -14,14 +14,10 @@ TEXINPUTS='.:..//:'
 export TEXINPUTS
 
 comp_file() {
-    pdflatex -halt-on-error  --interaction=nonstop --output-directory=out "$1"
+    lualatex -halt-on-error --interaction=nonstopmode --output-directory=out "$1"
 }
 biber_comp() {
     biber --input-directory=.. --output-directory=out main
 }
 
-if [[ -z "$draft" ]]; then
-    comp_file "$1"
-else
-    comp_file "$1" && biber_comp && comp_file "$1" && comp_file "$1"
-fi
+comp_file "$1" && biber_comp && comp_file "$1" && comp_file "$1"
