14a5156edd6f210c3ed880cecc10efc346b5de5e
Jhonny Lanzuisi <jalb97@gmail.com>
2024-05-14 10:53:26 -0400

    Improve build, change fonts, reorganize basicos.tex

--- debug.sh
+++ debug.sh
@@ -7,12 +7,34 @@ mkdir -p out
 flags='-halt-on-error --interaction=nonstopmode --output-directory=out'
 mainfile='main.tex'
 
+# Dump the preamble to fmt
+gen_fmt() {
+    if [ ! -f preamble.fmt ]; then
+        echo Generating fmt file.
+        pdflatex -ini '&pdflatex' preamble.tex
+    fi
+}
+
+# Clean up
+clean() {
+    rm -rf out/* preamble.fmt
+}
+
 # Quick compile
-pdflatex -draftmode $flags $mainfile
-pdflatex $flags $mainfile
+quick_comp() {
+    gen_fmt
+    fd '.*\.tex' | entr pdflatex --fmt=preamble $flags $mainfile
+}
 
 # Full compile (with bibiliopgraphy}
-# pdflatex -draftmode $flags $mainfile
-# biber --output-directory=out main
-# pdflatex -draftmode $flags $mainfile
-# pdflatex $flags $mainfile
+full_comp() {
+    gen_fmt
+    pdflatex --fmt=preamble -draftmode $flags $mainfile
+    biber --output-directory=out main
+    pdflatex --fmt=preamble -draftmode $flags $mainfile
+    pdflatex --fmt=preamble $flags $mainfile
+}
+
+clean
+full_comp
+quick_comp
