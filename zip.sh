a89ed7baefa441d5c600058f8a21ff4626f98214
Jhonny Lanzuisi <jalb97@gmail.com>
2024-08-21 10:02:05 -0400

    Add first proof of Kunen's theorem

--- zip.sh
+++ zip.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-zipfile="thesis_$(date +"%d-%m-%y_%H:%M").zip"
+zipfile="tesis-jhonny-lanzuisi_$(date +"%d-%m-%y_%H:%M").zip"
+
+# clean first
+rm *.aux *.bbl *.blg *.out *.xml *.toc *.fmt *.bcf *.log
 
 for file in *; do
     # Not a zip file
