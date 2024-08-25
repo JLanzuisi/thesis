317d312f0ca95e37b753b987fbe08a1997483743
Jhonny Lanzuisi <jalb97@gmail.com>
2024-08-25 12:58:35 -0400

    Switch to lualatex

--- debug.sh
+++ debug.sh
@@ -9,15 +9,11 @@ mainfile="main.tex"
 # git clean -fx
 rm -f main.pdf
 
-# Dump the preamble to fmt
-echo Generating fmt file.
-pdflatex $flags -ini '&pdflatex' '\def\initex{1}\input{preamble.tex}'
-
 # Get the compilation started
-pdflatex --fmt=preamble -draftmode $flags $mainfile
+lualatex -draftmode $flags $mainfile
 biber main
-pdflatex --fmt=preamble -draftmode $flags $mainfile
-pdflatex --fmt=preamble $flags $mainfile
+lualatex -draftmode $flags $mainfile
+lualatex $flags $mainfile
 
 # Keep updating the pdf on file changes
-fd '.*\.tex' | entr pdflatex --fmt=preamble $flags $mainfile
+fd '.*\.tex' | entr lualatex $flags $mainfile
--- zip.sh
+++ zip.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-zipfile="tesis-jhonny-lanzuisi_$(date +"%d-%m-%y_%H:%M").zip"
+zipfile="tesis-jhonny-lanzuisi_$(date | sed 's/ /_/g').zip"
 
-# clean first
-rm *.aux *.bbl *.blg *.out *.xml *.toc *.fmt *.bcf *.log
-
-for file in *; do
-    # Not a zip file
-    if [ -z $(echo $file | grep '.*\.zip') ]; then
-        zip -r $zipfile $file
-    fi
-done
+zip -f "$zipfile" *
