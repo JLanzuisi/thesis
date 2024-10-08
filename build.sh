3ac0501665a345d52b5e2a26f2c5a59384da3fea
Jhonny Lanzuisi <jalb97@gmail.com>
2024-10-08 17:31:31 -0400

    Fix more errors and work on slides

--- build.sh
+++ build.sh
@@ -2,26 +2,42 @@
 
 set -ex
 
-flags="-halt-on-error --interaction=nonstopmode"
-if [ -z $1 ]; then
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -f)
+            fast=1
+            shift # past argument
+            ;;
+        -m)
+            mainfile="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+    esac
+done
+
+if [ -z $mainfile ]; then
     mainfile=main.tex
-else
-    mainfile=$1
 fi
 lualatex=lualatex
 biber=biber
+flags="-halt-on-error --interaction=nonstopmode"
 
 bibmain=$(echo $mainfile | sed -e 's/\.tex//')
 
 # Clean up before anything
 rm -f main.pdf
 
-if [ "$1" == "-f" ]; then
-    $lualatex $flags $mainfile
-else
+if [ -z $fast ]; then
     # Full build
     $lualatex -draftmode $flags $mainfile
     $biber $bibmain
     $lualatex -draftmode $flags $mainfile
+    $lualatex $flags $mainfile
+else
     $lualatex $flags $mainfile
 fi
